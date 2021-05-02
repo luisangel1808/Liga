7d588c2b897fdf4d18c94d7b52ef148f6c60e05e
luisangel1808 <luisangel1808@hotmail.com>
2021-05-01 20:38:44 -0500

    Se añade autenticación y estilo en pantalla not found

---
+++ src/components/Login2.jsx
@@ -0,0 +1,33 @@
+import React,{useState} from 'react';
+import {auth} from '../firebase';
+import '../styles/components/Login.css'
+const Login2 = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const register = (e) =>{
+        e.preventDefault();
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((res)=>alert('Usuario registrado'));
+            
+    }
+    return (
+        <div className="Login">
+            <h1>Iniciar sesión</h1>
+            <form onSubmit={register}>
+                <input 
+                    onChange={(e)=>{setEmail(e.target.value)}}
+                    type="email"
+                    placeholder="email"
+                />
+                <input 
+                    onChange={(e)=>{setPassword(e.target.value)}}
+                    type="password"
+                    placeholder="contraseña"
+                />
+                <input type="submit" value="Iniciar sesión"/>
+            </form>
+
+        </div>
+    )
+}
+export default Login2
